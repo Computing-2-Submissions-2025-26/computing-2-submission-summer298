771cb4be66899624aee7c78e656b303bfa1136e4
summer298 <hak@ic.ac.uk>
2026-05-20 19:18:03 +0100

    Test

--- web-app/main.js
+++ web-app/main.js
@@ -1,1 +1,10 @@
 import R from "./ramda.js";
+
+const hello_message = document.getElementById("hello_message");
+
+hello_message.onclick = function () {
+    hello_message.textContent = "Bonjour!";
+
+}
+
+hello_message.textContent = "Bonjour!";
