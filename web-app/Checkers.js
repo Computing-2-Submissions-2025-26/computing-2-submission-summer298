863c617f2029c058e5f4dac48754274cc5164fd5
summer298 <hak@ic.ac.uk>
2026-06-03 20:42:58 +0100

    03/06

--- web-app/Checkers.js
+++ web-app/Checkers.js
@@ -53,6 +53,92 @@ Checkers.piece_strings = Object.freeze({
  * @returns {Checkers.Board} the empty board at the start
  */
 
-Checkers.empty_board = function (width = 8, height = 8) {
+/**Checkers.empty_board = function (width = 8, height = 8) {
     return R.repeat(R.repeat(0, height), width);
 };
+*/
+
+/**Create the starting 8x8 board
+ * @memberof Checkers
+ * @property 
+ * 
+ * 0 is empty square
+ * -1 is player 1 counter
+ * 1 is player 2 counter
+ */
+
+function build_board() {
+  game.innerHTML = "";
+  let black = 0;
+  let white = 0;
+  for (let i = 0; i < board.length; i++) {
+    const element = board[i];
+    let row = document.createElement("div"); // create div for each row
+    row.setAttribute("class", "row");
+
+    for (let j = 0; j < element.length; j++) {
+      const elmt = element[j];
+      let col = document.createElement("div"); // create div for each case
+      let piece = document.createElement("div");
+      let caseType = "";
+      let occupied = "";
+
+      if (i % 2 === 0) {
+        if (j % 2 === 0) {
+          caseType = "Whitecase";
+        } else {
+          caseType = "blackCase";
+        }
+      } else {
+        if (j % 2 !== 0) {
+          caseType = "Whitecase";
+        } else {
+          caseType = "blackCase";
+        }
+      }
+
+      // add the piece if the case isn't empty
+      if (board[i][j] === 1) {
+        occupied = "whitePiece";
+      } else if (board[i][j] === -1) {
+        occupied = "blackPiece";
+      } else {
+        occupied = "empty";
+      }
+
+      piece.setAttribute("class", "occupied " + occupied);
+
+      // set row and colum in the case
+      piece.setAttribute("row", i);
+      piece.setAttribute("column", j);
+      piece.setAttribute("data-position", i + "-" + j);
+
+      //add event listener to each piece
+      piece.addEventListener("click", movePiece);
+
+      col.appendChild(piece);
+
+      col.setAttribute("class", "column " + caseType);
+      row.appendChild(col);
+
+      // counter number of each piece
+      if (board[i][j] === -1) {
+        black++;
+      } else if (board[i][j] === 1) {
+        white++;
+      }
+
+      //display the number of piece for each player
+      displayCounter(black, white);
+    }
+
+    game.appendChild(row);
+  }
+
+  if (black === 0 || white === 0) {
+    modalOpen(black);
+  }
+}
+
+
+export default Object.freeze(Checkers);
--- web-app/main.js
+++ web-app/main.js
@@ -1,2 +1,53 @@
 import R from "./ramda.js";
 import Checkers from "./Checkers.js";
+
+
+const image_sources = [
+    "./assets/soil.png",
+    "./assets/greengrass.png",
+    "./assets/cat.png",
+    "./assets/lion.png",
+    "./assets/dog.png",
+    "./assets/wolf.png",
+];
+
+const image_alts = [
+    "Empty brown square",
+    "Empty green square",
+    "Cat",
+    "Lion",
+    "Dog",
+    "Wolf"
+];
+
+const player_types = {
+    "1" : "Felines",
+    "2" : "Canines"
+}
+
+const game_board = document.getElementById("game_board");
+
+
+const result_dialog = document.getElementById("result_dialog"); 
+
+// Home player / away player are displayed on the left / right sidebars.
+// Player 1 / Player 2 go first / second. These alternate each game.
+let home_player = document.getElementById("home_name").value;
+let away_player = document.getElementById("away_name").value;
+let home_player_type;
+
+let board = [
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+];
+
+let game = document.getElementById("game");
+let player = 1;
+
+build_board()
