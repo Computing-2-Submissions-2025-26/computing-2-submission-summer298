22d3aa7e8791a0f11ead8a97853f800e1907ac83
summer298 <hak@ic.ac.uk>
2026-06-23 13:13:15 +0100

    checkingcommitworks

---
+++ web-app/deletelater.js
@@ -0,0 +1,148 @@
+const Checkers = Object.create(null);
+
+/**
+ * A Board is an 8 by 8 grid of squares.
+ * It is implemented as an array of rows.
+ * Each square contains a piece or an empty position.
+ * @memberof Checkers
+ * @typedef {Checkers.Piece_or_empty[][]} Board
+ */
+const board = [
+    null, "cat1", null, "cat2", null, "cat3", null, "cat4",
+    "cat5", null, "cat6", null, "cat7", null, "cat8", null,
+    null, "cat9", null, "cat10", null, "cat11", null, "cat12",
+    null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null,
+    "dog1", null, "dog2", null, "dog3", null, "dog4", null,
+    null, "dog5", null, "dog6", null, "dog7", null, "dog8",
+    "dog9", null, "dog10", null, "dog11", null, "dog12", null
+]
+
+/**
+ * A piece is a checker that belongs to one of the two players.
+ * Regular pieces can move diagnonally forwards, while kings can move diagonally forwards and backwards.
+ * @memberof Checkers
+ * @typedef {(1 | 2 | 3 | 4)} Piece
+ */
+
+/**
+ * Either a checker piece or an empty square.
+ * @memberof Checkers
+ * @typedef {(Checkers.Piece | 0)} Piece_or_empty
+ */
+
+/**
+ * Template piece strings for {@link Checkers.to_string_with_pieces}.
+ * @memberof Checkers
+ * @enum {string[]}
+ * @property {string[]} default ["0", "1", "2", "3", "4"]
+ * Displays pieces by their index.
+ * @property {string[]} tokens ["▪️", "🔴", "🔵", "🟥", "🟦"] 
+ * Displays pieces as checker symbols.
+ * @property {string[]} pets ["▪️", "🐈", "🐕", "🦁", "🐺"]
+ */
+Checkers.piece_strings = Object.freeze({
+    "default" : ["0", "1", "2", "3", "4"],
+    "tokens" : ["▪️", "🔴", "🔵", "🟥", "🟦"],
+    "pets" : ["▪️", "🐈", "🐕", "🦁", "🐺"]
+});
+
+/**
+ * Create empty 8x8 board
+ * @memberof Checkers
+ * @function
+ * @param {number} [width] = 8
+ * @param {number} [height] = 8
+ * @returns {Checkers.Board} the empty board at the start
+ */
+
+/**Checkers.empty_board = function (width = 8, height = 8) {
+    return R.repeat(R.repeat(0, height), width);
+};
+*/
+
+/**Create the starting 8x8 board
+ * @memberof Checkers
+ * @property 
+ * 
+ * 0 is empty square
+ * -1 is player 1 counter
+ * 1 is player 2 counter
+ */
+
+function build_board() {
+  game.innerHTML = "";
+  let black = 0;
+  let white = 0;
+  
+  for (let i = 0; i < board.length; i++) {
+    const element = board[i];
+    let row = document.createElement("div"); // create div for each row
+    row.setAttribute("class", "row");
+
+    for (let j = 0; j < element.length; j++) {
+      const elmt = element[j];
+      let col = document.createElement("div"); // create div for each case
+      let piece = document.createElement("div");
+      let caseType = "";
+      let occupied = "";
+
+      if (i % 2 === 0) {
+        if (j % 2 === 0) {
+          caseType = "Whitecase";
+        } else {
+          caseType = "blackCase";
+        }
+      } else {
+        if (j % 2 !== 0) {
+          caseType = "Whitecase";
+        } else {
+          caseType = "blackCase";
+        }
+      }
+
+      // add the piece if the case isn't empty
+      if (board[i][j] === 1) {
+        occupied = "whitePiece";
+      } else if (board[i][j] === -1) {
+        occupied = "blackPiece";
+      } else {
+        occupied = "empty";
+      }
+
+      piece.setAttribute("class", "occupied " + occupied);
+
+      // set row and colum in the case
+      piece.setAttribute("row", i);
+      piece.setAttribute("column", j);
+      piece.setAttribute("data-position", i + "-" + j);
+
+      //add event listener to each piece
+      piece.addEventListener("click", movePiece);
+
+      col.appendChild(piece);
+
+      col.setAttribute("class", "column " + caseType);
+      row.appendChild(col);
+
+      // counter number of each piece
+      if (board[i][j] === -1) {
+        black++;
+      } else if (board[i][j] === 1) {
+        white++;
+      }
+
+      //display the number of piece for each player
+      displayCounter(black, white);
+    }
+
+    game.appendChild(row);
+  }
+
+  if (black === 0 || white === 0) {
+    modalOpen(black);
+  }
+}
+
+
+export default Object.freeze(Checkers);
